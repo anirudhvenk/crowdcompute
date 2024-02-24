93afd98c87661a2195da7c83b8dfc8d5c25d5513
Anirudh Venkatraman <anirudhv05ca@gmail.com>
2024-02-23 23:30:26 -0600

    added homepage

---
+++ public/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+
+require('@electron/remote/main').initialize()
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        enableRemoteModule: true
+    }
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+})
+  
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
--- src/App.js
+++ src/App.js
@@ -3,21 +3,65 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='side-nav'>
+        <div className='logo' />
+        <div className='nav-list'>
+          <div className='nav-item'>
+            <div className='leading-icon-nav-link'>
+              <div className='leading-icon'>
+                <div className='user'>
+                  <div className='iconly-bold-user' />
+                </div>
+              </div>
+              <div className='nav-link'>
+                <span className='dashboard'>Host</span>
+              </div>
+            </div>
+          </div>
+          <div className='nav-item-1'>
+            <div className='leading-icon-nav-link-2'>
+              <div className='leading-icon-3'>
+                <div className='filter'>
+                  <div className='iconly-bold-filter' />
+                </div>
+              </div>
+              <div className='nav-link-4'>
+                <span className='dashboard-5'>Upload</span>
+              </div>
+            </div>
+          </div>
+          <div className='nav-item-6'>
+            <div className='leading-icon-nav-link-7'>
+              <div className='leading-icon-8'>
+                <div className='setting'>
+                  <div className='iconly-bold-setting' />
+                </div>
+              </div>
+              <div className='nav-link-9'>
+                <span className='dashboard-a'>Profile</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex-column-ec'>
+        <button className='rectangle'>
+          <div className='upload-logo' />
+        </button>
+        <div className='upload-files'>
+          <span className='upload-files-b'>
+            Upload Files
+            <br />
+          </span>
+          <span className='upload-files-c'>
+            <br />
+          </span>
+          <span className='upload-files-d'>
+            Upload a zip file of your code and training data
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
