6e2530abb592ddfbff5da764a125f091c3f8cc69
adame <elbahey2@illinois.edu>
2024-02-24 18:05:39 -0800

    fixed prodile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,47 +4,45 @@ import "./Profile.css";
 
 export default function Profile () {
   return (
-    <div className="profile">
-      
-        <div className="profile-sec">
-          <div className="overlap-group">
-            <div className="rectangle" />
-            <div className="text-wrapper">Servers Hosted</div>
-          </div>
-          <div className="overlap">
-            <img className="arrow" alt="Arrow" src="arrow-1.svg" />
-            <div className="rectangle-2" />
-          </div>
-        </div>
-        <div className="overlap-wrapper">
-          <div className="overlap-2">
-            <div className="rectangle-3" />
-            <img className="img" alt="Arrow" src="arrow-1-3.svg" />
-            <div className="rectangle-4" />
-            <div className="text-wrapper-2">Wallet</div>
-          </div>
-        </div>
-        <div className="profile-sec-2">
-          <div className="overlap-3">
-            <div className="rectangle-3" />
-            <div className="text-wrapper-3">Models Trained</div>
-          </div>
-          <div className="overlap">
-            <img className="arrow" alt="Arrow" src="arrow-1-2.svg" />
-            <div className="rectangle-2" />
-          </div>
-        </div>
-        <div className="profile-sec-3">
-          <div className="overlap-3">
-            <div className="rectangle-3" />
-            <div className="text-wrapper">Sent Projects</div>
-          </div>
-          <div className="overlap-4">
-            <img className="arrow-2" alt="Arrow" src="image.svg" />
-            <div className="rectangle-2" />
-          </div>
-        </div>
-      </div>
-    
-  );
+<div className="profile">
+<div className="div-2">
+
+</div>
+<div className="profile-sec">
+<div className="overlap-group">
+<div className="rectangle" />
+<div className="text-wrapper">Servers Hosted</div>
+</div>
+<div className="rectangle-2" />
+
+<div className="profile-sec-2">
+<div className="overlap">
+<div className="rectangle-3" />
+<div className="text-wrapper-2">Wallet</div>
+</div>
+<div className="rectangle-2" />
+</div>
+<div className="profile-sec-3">
+<div className="overlap">
+<div className="rectangle-3" />
+<div className="text-wrapper">Sent Projects</div>
+</div>
+<div className="rectangle-4" />
+</div>
+<div className="group">
+<div className="div-wrapper">
+<div className="text-wrapper-3">Profile</div>
+</div>
+</div>
+<div className="profile-sec-4">
+<div className="overlap">
+<div className="rectangle-3" />
+<div className="text-wrapper-4">Models Trained</div>
+</div>
+<div className="rectangle-2" />
+</div>
+</div>
+</div>
+);
+
 };
