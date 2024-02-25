275912b99c478b7c4d65ed17e314581487fbbc9b
Anirudh Venkatraman <anirudhv05ca@gmail.com>
2024-02-24 21:37:38 -0600

    added progress bar

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,10 +1,25 @@
 import upload_logo from '../assets/real_upload.svg'
 import './Upload.css'
 import React from "react"
+import { useState, useEffect } from 'react';
 const { ipcRenderer } = window.require('electron');
 
 function Upload() {
     const fileInputRef = React.useRef(null);
+
+    const [operationFinished, setOperationFinished] = useState(false);
+
+    useEffect(() => {
+        ipcRenderer.on('uploaded-files', (event, message) => {
+            if (message === 'uploaded') {
+                setOperationFinished(true);
+            }
+        });
+
+        return () => {
+            ipcRenderer.removeAllListeners('operation-finished');
+        };
+    }, []);
 
     const triggerFileInput = () => {
         fileInputRef.current.click();
@@ -19,28 +34,37 @@ function Upload() {
 
     return (
         <>
-            <div className="box" onClick={triggerFileInput}>
-                <input 
-                    type="file"
-                    ref={fileInputRef}
-                    onChange={handleFileChange}
-                    style={{ display: 'none' }}
-                    accept=".zip"
-                />
-                <img className="upload-logo" alt="Upload logo" src={upload_logo} />
-            </div>
-            <div className="label">
-                <div className="flexcontainer">
-                    <p className="text">
-                        <span className="upload-wrapper">Upload Files</span>
-                        <br />
-                        <span className="span">Upload a zip file of your code and training data</span>
-                    </p>
+        {operationFinished ? (
+            <>
+                <progress className="progress" value={null}>
+                </progress>
+                <span className="progress-text">Training model</span>
+            </>
+        ) : (
+            <>
+                <div className="box" onClick={triggerFileInput}>
+                    <input 
+                        type="file"
+                        ref={fileInputRef}
+                        onChange={handleFileChange}
+                        style={{ display: 'none' }}
+                        accept=".zip"
+                    />
+                    <img className="upload-logo" alt="Upload logo" src={upload_logo} />
                 </div>
-            </div>
+                <div className="label">
+                    <div className="flexcontainer">
+                        <p className="text">
+                            <span className="upload-wrapper">Upload Files</span>
+                            <br />
+                            <span className="span">Upload a zip file of your code and training data</span>
+                        </p>
+                    </div>
+                </div>
+            </>
+        )}
         </>
-        
-    )
+    );
 }
 
 export default Upload;
